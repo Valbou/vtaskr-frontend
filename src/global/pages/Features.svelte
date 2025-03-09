c57a9568e6d9f513b060b4bc95b48b2fedb7e301
Valbou <contact@valbou.fr>
2025-03-09 14:47:25 +0100

    feat: translate presentation and features pages

--- src/global/pages/Features.svelte
+++ src/global/pages/Features.svelte
@@ -1,23 +1,27 @@
 <script>
     import { Link } from 'svelte-routing'
+
+    import { getText } from "../../i18n/services/translation.js"
+
+    import Trans from "../../i18n/components/Trans.svelte"
 </script>
 
 <section class="features">
-    <h1>vTaskr Features - Task Manager</h1>
-    <p>vTaskr is an open source task manager that can help you to organize and memorize tasks.</p>
-    <p>It provide some features to permit collaborative projects:</p>
+    <h1><Trans textKey="global:features:title" /></h1>
+    <p><Trans textKey="global:features:help_you" /></p>
+    <p><Trans textKey="global:features:collaborative_project" /></p>
     <div>
         <ul>
-            <li>Create and manage tasks</li>
-            <li>Eisenhower matrix can help you to priorize tasks</li>
-            <li>Create groups and invite collaborators</li>
-            <li>Use tags to organize tasks in a group</li>
-            <li>Use reminder by email or telegram</li>
-            <li>Privacy by design</li>
-            <li>Many others features are comming... <a href="https://github.com/Valbou/vtaskr-frontend" title="Link to Github vTaskr" target="_blank" rel="external">follow us on github</a></li>
+            <li><Trans textKey="global:features:create_and_manage" /></li>
+            <li><Trans textKey="global:features:eisenhower_matrix" /></li>
+            <li><Trans textKey="global:features:groups_and_collaborators" /></li>
+            <li><Trans textKey="global:features:use_tags" /></li>
+            <li><Trans textKey="global:features:use_reminder" /></li>
+            <li><Trans textKey="global:features:privacy_by_design" /></li>
+            <li><Trans textKey="global:features:many_others_features" /> <a href="https://github.com/Valbou/vtaskr-frontend" title={getText("global:features:title_github")} target="_blank" rel="external"><Trans textKey="global:features:follow_us" /></a></li>
         </ul>
-        <p>vTaskr is free for all (SaaS and on-premise), but this can evolve in the futur for large users in SaaS mode only (cloud). It depends on user's generousity, if it's enough to maintain vTaskr infrastructure and code.</p>
+        <p><Trans textKey="global:features:free_for_all" /></p>
     </div>
-    <p class="center"><Link class="button" to="/register">Register</Link> or <Link class="button" to="/login">Login</Link> for free.</p>
-    <p>You need more features, you experience an issue, please <a href="https://github.com/Valbou/vtaskr-frontend/issues" target="_blank" rel="external">create an issue on github project</a></p>
+    <p class="center"><Link class="button" to="/register"><Trans textKey="global:register" /></Link> <Trans textKey="global:about:or" /> <Link class="button" to="/login"><Trans textKey="global:login" /></Link> <Trans textKey="global:features:for_free" /></p>
+    <p><Trans textKey="global:features:more_features" /> <a href="https://github.com/Valbou/vtaskr-frontend/issues" target="_blank" rel="external"><Trans textKey="global:features:create_issues" /></a></p>
 </section>
--- src/global/pages/Presentation.svelte
+++ src/global/pages/Presentation.svelte
@@ -1,6 +1,10 @@
 <script>
     import { Link } from 'svelte-routing'
+
+    import { getText } from "../../i18n/services/translation.js"
+
     import DarkTasksDesktop from '/post-it-background-blue.jpg'
+    import Trans from "../../i18n/components/Trans.svelte"
 </script>
 
 <section class="presentation">
@@ -9,15 +13,14 @@
             <img src={ DarkTasksDesktop } alt="Desktop with many task sheets." />
         </div>
         <figcaption>
-            <h1>Free your Mind with vTaskr !</h1>
-            <p>
-                Make your life easier and less stressful with a free open-source task manager.
-            </p>
+            <h1><Trans textKey="global:presentation:title" /></h1>
+            <p><Trans textKey="global:presentation:paragraph" /></p>
         </figcaption>
     </figure>
 
     <p class="center">
-        <Link class="button" to="/register">Register</Link>
-        or <Link class="button" to="/login">Login</Link>
+        <Link class="button" to="/register"><Trans textKey="global:register" /></Link>
+        <Trans textKey="global:about:or" />
+        <Link class="button" to="/login"><Trans textKey="global:login" /></Link>
     </p>
 </section>
